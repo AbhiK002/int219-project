655ac7610ff59ee52ca80e5b5630a4d8fddcdd95
Abhineet Kelley <abhineet.kelley@gmail.com>
2023-10-15 17:54:17 +0530

    cart preview done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import * as $ from 'jquery';
 
 @Component({
-  selector: 'app-root', // This defines the HTML element where the component will be rendered
-  templateUrl: './app.component.html', // The HTML template for the component
-  styleUrls: ['./app.component.css'] // The CSS styles for the component
+  selector: 'app-root', 
+  templateUrl: './app.component.html', 
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Gismos - Online Shopping'; // A sample title for your application
+  title = 'Gismos - Online Shopping';
 
   toggleCartPreview() {
     $(".cart-preview").toggleClass("enabled");
