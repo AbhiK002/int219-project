f6f17aed233d909101c1c11e705d94508455b1eb
Abhineet Kelley <abhineet.kelley@gmail.com>
2023-10-18 14:57:29 +0530

    profile page commit 1

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
-
+  currentlyViewing: number = 0;
+  switchScreen(ind: number) {
+    this.currentlyViewing = ind;
+  }
 }
