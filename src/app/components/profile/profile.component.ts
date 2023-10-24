c1e0f7bb9d37cee61c5b484d62ecaa628fcc147a
Abhineet Kelley <abhineet.kelley@gmail.com>
2023-10-25 01:35:37 +0530

    profile page done

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-profile',
@@ -9,5 +10,17 @@ export class ProfileComponent {
   currentlyViewing: number = 0;
   switchScreen(ind: number) {
     this.currentlyViewing = ind;
+  }
+
+  globNotifs = true;
+  globNotifs2 = true;
+  toggleGlob() {
+    this.globNotifs = !this.globNotifs;
+  }
+  toggleGlob2() {
+    this.globNotifs2 = !this.globNotifs2;
+  }
+  resetChecks() {
+    $(".preference-item .check").prop("checked", false);
   }
 }
