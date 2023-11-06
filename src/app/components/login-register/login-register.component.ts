4d92ceaa23a02958c6dcf50cf45d928a42c74991
Abhineet Kelley <abhineet.kelley@gmail.com>
2023-11-06 10:58:49 +0530

    added form validation

--- src/app/components/login-register/login-register.component.ts
+++ src/app/components/login-register/login-register.component.ts
@@ -19,12 +19,48 @@ export class LoginRegisterComponent {
     this.isRegistering = !this.isRegistering;
   }
 
+  validateForm() {
+    const emailField = $('#loginEmail').val()?.toLocaleString().trim();
+    const passField = $('#loginPassword').val()?.toLocaleString().trim();
+    const cPassField = $('#loginConfirmPassword').val()?.toLocaleString().trim();
+
+    if (!emailField || emailField.length == 0) {
+      alert("Please fill the Email first");
+      return false;
+    }
+    if (!passField || passField.length == 0) {
+      alert("Please fill the Password");
+      return false;
+    }
+    if (this.isRegistering && (!cPassField || cPassField.length == 0)) {
+      alert("Please confirmt the password");
+      return false;
+    }
+
+    if (!emailField.match(/[a-z0-9\._%+!$&*=^|~#%'`?{}/\-]+@([a-z0-9\-]+\.){1,}([a-z]{2,16})/)) {
+      alert("Invalid email");
+      return false;
+    }
+    if (passField.length < 6) {
+      alert("Password should contain at least 6 characters");
+      return false;
+    }
+    if (this.isRegistering && cPassField !== passField) {
+      alert("Confirm password doesn't match the entered password");
+      return false;
+    }
+
+    return true;
+  }
+
   login(): void {
+    if (!this.validateForm()) return;
     alert("Logged In!");
     this.router.navigate(['/']);
   }
 
   register(): void {
+    if (!this.validateForm()) return;
     alert("Registered!");
     this.router.navigate(['/']);
   }
